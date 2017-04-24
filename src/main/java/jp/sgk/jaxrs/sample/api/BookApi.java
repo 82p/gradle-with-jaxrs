52b182b094df76c27bda1abc14bb3bfeb0eea381
haniyama_wataru <haniyama_wataru@sgk.jp>
2017-04-24 12:06:31 +0900

    add books api

--- src/main/java/jp/sgk/jaxrs/sample/api/BookApi.java
+++ src/main/java/jp/sgk/jaxrs/sample/api/BookApi.java
@@ -39,6 +39,12 @@ public class BookApi {
 	public List<Book> getBook(@PathParam("name") String name){
 		return factory.findByName(name);
 	}
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Book> getBooks(){
+		return factory.getList();
+	}
+
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
