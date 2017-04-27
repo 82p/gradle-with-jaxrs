2f6fea5b85f47549742e7bacef1382debbf56f95
haniyama_wataru <haniyama_wataru@sgk.jp>
2017-04-27 11:15:20 +0900

    conflict 修正ミスを修正

--- src/main/java/jp/sgk/jaxrs/sample/api/BookApi.java
+++ src/main/java/jp/sgk/jaxrs/sample/api/BookApi.java
@@ -45,11 +45,6 @@ public class BookApi {
 	public List<Book> getBook(@PathParam("name") String name){
 		return factory.findByName(name);
 	}
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public List<Book> getBooks(){
-		return factory.getList();
-	}
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON+";charset=utf-8")
